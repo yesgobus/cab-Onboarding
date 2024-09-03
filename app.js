b1b4d6a0bba370c105a5f47cdf61835755474705
Dhairya99999 <dhairya.jan9@gmail.com>
2024-09-03 18:08:18 +0530

    error view

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use((req, res, next) => {
 });
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static('public'));
