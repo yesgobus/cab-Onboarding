77a0ee584c559824854b72e71a795458c4d99301
Dhairya99999 <dhairya.jan9@gmail.com>
2024-09-04 13:52:31 +0530

    restarttst

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ io.on('connection', (socket) => {
       if(customer.on_going_ride_id){
         const ongoingRide = customer.on_going_ride_id;
   
-        io.to(customer.socketId).emit('pickup-status', { ride_id: customer.on_going_ride_id._id, ...ongoingRide.toObject() });
+        io.to(customer.socketId).emit('trip-driver-accepted', { ride_id: customer.on_going_ride_id._id, ...ongoingRide.toObject() });
        }
        else{
         console.log("No ongoing ride for the registered customer")
